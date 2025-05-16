ba8cbde4cc18c2acb15c0a9194ad624f17939e31
TSK-3 <thekarthiktangirala@gmail.com>
2025-05-16 19:57:37 +0530

    props

---
+++ section_2/src/components/Entry.jsx
@@ -0,0 +1,23 @@
+export default function Entry(props) {
+  console.log(props);
+  return (
+    <article className="entry">
+      <div className="image-container">
+        <img
+          src={props.img.src}
+          alt={props.img.alt}
+          width={125}
+          className="main_image"
+        />
+      </div>
+      <div>
+        <img src="/marker.png" alt="Marker"  className="marker"/>
+        <span>{props.country}</span>
+        <a href= {props.googleMapsLink}>View on Google Maps</a>
+        <h2>{props.title}</h2>
+        <p className="dates">{props.dates}</p>
+        <p className="description">{props.text}</p>
+      </div>
+    </article>
+  );
+}
---
+++ section_2/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="/globe.png" alt="Globe" />
+            <h1>My Travel Journal</h1>
+        </header>
+    )
+}
+
+
+
